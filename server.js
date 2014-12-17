c0e0a0549b2a8b892c0a16dc9a4b11655b86cc36
Ryan Carter <ryan@hotelengine.com>
2014-12-17 16:37:33 -0700

    Nodebot is born. Your tiny friendly node, connect, serve-static webserver

---
+++ server.js
@@ -0,0 +1,15 @@
+/*  NODEBOT IS YOUR TINY FRIENDLY SERVER. DO NOT BE ALARMED. :)
+*   
+*   1. If you have node/npm, install connect and serve-static in terminal (without the $): 
+*     $ npm install connect serve-static
+*
+*   2. Start nodebot in terminal (without the $): 
+*     $ node server
+*
+*   Note: Shutdown with Ctrl + C
+*/
+var connect = require('connect');
+var serveStatic = require('serve-static');
+var port = 8080;
+connect().use(serveStatic(__dirname)).listen(port);
+console.log('Nodebot is now serving your files at http://locahost:'+port+'.\r\nShutdown with Ctrl+C');
